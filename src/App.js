748f9e90f4c0ffd9d5af9c8d7fd5468c88a97943
LauraPlaza <0619967435@uma.es>
2022-12-16 10:17:42 +0100

    puedo estar tranquila

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> hola
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId="924104507344-il7n3cpre0kjl7pna5bncbipidsrdcno.apps.googleusercontent.com">
+        <div>hola den toocvcvzcvdf</div>
+
+    </GoogleOAuthProvider>
   );
 }
 
