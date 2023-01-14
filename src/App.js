6af912aded55ac5444108fb87cca95d861ce801e
LauraPlaza <0619967435@uma.es>
2023-01-14 11:11:44 +0100

    ddkjf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import React, { useEffect, useState }  from 'react';
+import React, {  useState }  from 'react';
 import MainAppBar from './Components/MainAppBar/MainAppBar';
 import Main from './pages/Main/Main';
 import Logs from './pages/Logs/Logs';
@@ -13,7 +13,7 @@ import Logs from './pages/Logs/Logs';
 
 
 function App() {
-  const clientId='924104507344-il7n3cpre0kjl7pna5bncbipidsrdcno.apps.googleusercontent.com'
+  /*const clientId='924104507344-il7n3cpre0kjl7pna5bncbipidsrdcno.apps.googleusercontent.com'*/
   const [user,setUser] = useState({});
   
 
